e9da9a1e056840108b3835587aa5fb7c76c3b973
YUANJA3 <yuanja3@oocl.com>
2025-09-16 14:27:51 +0800

    fix:解决跨域问题

---
+++ src/main/java/com/bootcamp/todolist/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.bootcamp.todolist.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // 允许所有来源
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
