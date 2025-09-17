8537dde467284d8f7f552cef8b3e987d42a1d04f
YUANJA3 <yuanja3@oocl.com>
2025-09-17 13:21:11 +0800

    ci:fix ci's problems

--- src/main/resources/db/migration/V202509171304__Create_table_todo.sql
+++ src/main/resources/db/migration/V202509171304__Create_table_todo.sql
@@ -1,8 +1,6 @@
-create table if not exists todo_list.todo
-(
-    id   bigint auto_increment
-    primary key,
-    text varchar(255) default '""' null,
-    done tinyint(1)   default 0    not null
-    );
+create table todo (
+                      id   bigint auto_increment primary key,
+                      text varchar(255) default null,
+                      done tinyint default 0 not null
+);
 
--- src/test/java/com/bootcamp/todolist/controller/TodoListControllerTest.java
+++ src/test/java/com/bootcamp/todolist/controller/TodoListControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TodoListControllerTest {
