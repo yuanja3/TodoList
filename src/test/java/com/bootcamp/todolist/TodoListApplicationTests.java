91e5d87305d1c50f60186d8f1f2f0276fcc6c410
YUANJA3 <yuanja3@oocl.com>
2025-09-17 13:27:43 +0800

    ci:fix ci's problems

--- src/test/java/com/bootcamp/todolist/TodoListApplicationTests.java
+++ src/test/java/com/bootcamp/todolist/TodoListApplicationTests.java
@@ -2,6 +2,9 @@ package com.bootcamp.todolist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class TodoListApplicationTests {
